35d1d1a74483818abb9a9e65c733605e0b58ac66
Michael <ferrellmichael7@gmail.com>
2017-12-22 01:45:20 -0800

    Revamp Cabinet.js in Grunt, Browserify, Babel, and ES6

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import StorageFactory from './StorageFactory'
+import { storageSupported } from './utilities'
+
+if (storageSupported()) {
+	const cabinet = new StorageFactory('local')
+	cabinet.session = new StorageFactory('session')
+	window.Cabinet = cabinet
+}
